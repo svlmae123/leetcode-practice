247dd6bd6e04bf3b75580717ccd29d7877272d42
salma asbai <salmaasbai91@gmail.com>
2025-10-27 20:55:55 +0100

    Time: 0 ms (100.00%) | Memory: 41.9 MB (50.73%) - LeetSync

---
+++ 66-plus-one/plus-one.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+
+        int n = digits.length;
+
+        for(int i=n-1; i>=0; i--) {
+
+            if(digits[i]<9) {
+                digits[i]++;
+                return digits;
+            }
+                digits[i] = 0;
+                    
+            }
+
+            int[] results = new int[n+1];
+            results[0] = 1;
+            return results;
+        }
+
+        
+    }
